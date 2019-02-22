c04a81b49a37f1a2c9e13e62a4a66463646b9495
Jamie Pham <jtkp66@gmail.com>
2019-02-22 10:27:51 -0800

    Bulk addition of action creators for all app CRUD operations.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,13 @@
 import posts from '../apis/posts';
-import { SIGN_IN, SIGN_OUT } from './types';
+import { 
+    SIGN_IN, 
+    SIGN_OUT, 
+    CREATE_POST,
+    FETCH_POSTS,
+    FETCH_POST,
+    DELETE_POST,
+    EDIT_POST
+ } from './types';
 
 export const signIn = (userId) => {
     return {
@@ -15,5 +23,31 @@ export const signOut = () => {
 };
 
 export const createPost = formValues => async dispatch => {
-    posts.post('/posts', formValues);
+    const response = await posts.post('/posts', formValues);
+
+    dispatch({ type: CREATE_POST, payload: response.data });
+};
+
+export const fetchPosts = () => async dispatch => {
+    const response = await posts.get('/posts');
+
+    dispatch({ type: FETCH_POSTS, payload: response.data });
+};
+
+export const fetchPost = (id) => async dispatch => {
+    const response = await posts.get(`/streams/${id}`);
+
+    dispatch({ type: FETCH_POST, payload: response.data });
+};
+
+export const editPost = (id, formValues) => async dispatch => {
+    const response = await posts.put(`/posts/${id}`, formValues);
+
+    dispatch({ type: EDIT_POST, payload: response.data});
+};
+
+export const deletePost = (id) => async dispatch => {
+    await posts.delete(`/streams/${id}`);
+
+    dispatch({ type: DELETE_POST, payload: id });
 };
