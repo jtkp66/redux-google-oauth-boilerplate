7eb65a675d905f1580ebb1347517a87df0eb8e28
Jamie Pham <jtkp66@gmail.com>
2019-02-21 17:35:51 -0800

    Able to render redux form fields.

--- src/components/posts/PostCreate.js
+++ src/components/posts/PostCreate.js
@@ -1,11 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-export default class PostCreate extends Component {
-  render() {
+class PostCreate extends Component {
+  renderInput(formProps) {
     return (
-      <div>
-        PostCreate
+      <div className="field">
+      <label>{ formProps.label }</label>
+        <input {...formProps.input} />
       </div>
     )
   }
+
+  // renderInput({ input }) {
+  //   return <input {...input} />;
+  // }
+
+  render() {
+    console.log(this.props);
+    return (
+      <form className="ui form">
+        <Field name="title" component={this.renderInput} label="Enter a Title" />
+        <Field name="description" component={this.renderInput} label="Enter a Description" />
+      </form>
+    )
+  }
 }
+
+export default reduxForm({
+  form: 'postCreate'
+})(PostCreate);
