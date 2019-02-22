bfa32cb73d1e4ee2310e4fda2a42a59994105423
Jamie Pham <jtkp66@gmail.com>
2019-02-22 11:05:35 -0800

    Added reducers to handle CRUD operations, and installed lodash to omit posts, and mapKeys to merge list of records.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
+import postReducer from './postReducer';
 
 export default combineReducers({
     auth: authReducer,
-    form: formReducer
+    form: formReducer,
+    posts: postReducer
 });
