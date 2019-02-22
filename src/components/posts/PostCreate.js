09e9679aa2640d2a0f292edb84501c2248766419
Jamie Pham <jtkp66@gmail.com>
2019-02-21 23:54:17 -0800

    Added highlighting for errored fields.

--- src/components/posts/PostCreate.js
+++ src/components/posts/PostCreate.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import { Field, reduxForm, formValues } from 'redux-form';
+import { Field, reduxForm } from 'redux-form';
 
 class PostCreate extends Component {
-  renderInput(formProps) {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      )
+    }
+  }
+
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : '' }`;
     return (
-      <div className="field">
-        <label>{formProps.label}</label>
-        <input {...formProps.input} />
-        <div>{formProps.meta.error}</div>
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
       </div>
     )
   }
 
-  // renderInput({ input }) {
+  // renderInput({ input, label, meta }) {
   //   return <input {...input} />;
   // }
 
@@ -23,7 +34,7 @@ class PostCreate extends Component {
   render() {
     console.log(this.props);
     return (
-      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
         <Field name="title" component={this.renderInput} label="Enter a Title" />
         <Field name="description" component={this.renderInput} label="Enter a Description" />
         <button className="ui button primary">Submit</button>
