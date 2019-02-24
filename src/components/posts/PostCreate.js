acb20842e3ca1ef459891c3317b052ed1c877e15
Jamie Pham <jtkp66@gmail.com>
2019-02-24 14:36:04 -0800

    Created PostForm component, and wired it to PostCreate component.

--- src/components/posts/PostCreate.js
+++ src/components/posts/PostCreate.js
@@ -1,67 +1,22 @@
 import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { createPost } from '../../actions';
+import PostForm from './PostForm';
 
 class PostCreate extends Component {
-  renderError({ error, touched }) {
-    if (touched && error) {
-      return (
-        <div className="ui error message">
-          <div className="header">{error}</div>
-        </div>
-      )
-    }
-  }
-
-  renderInput = ({ input, label, meta }) => {
-    const className = `field ${meta.error && meta.touched ? 'error' : '' }`;
-    return (
-      <div className={className}>
-        <label>{label}</label>
-        <input {...input} autoComplete="off" />
-        {this.renderError(meta)}
-      </div>
-    )
-  }
-
-  // renderInput({ input, label, meta }) {
-  //   return <input {...input} />;
-  // }
-
   onSubmit = (formValues) => {
     this.props.createPost(formValues);
   }
 
   render() {
     console.log(this.props);
-    return (
-      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
-        <Field name="title" component={this.renderInput} label="Enter a Title" />
-        <Field name="description" component={this.renderInput} label="Enter a Description" />
-        <button className="ui button primary">Submit</button>
-      </form>
+    return(
+      <div>
+        <h3>Create a Post</h3>
+        <PostForm onSubmit={this.onSubmit} />
+      </div>
     );
   }
 }
 
-const validate = (formValues) => {
-  const errors = {};
-
-  if (!formValues.title) {
-    errors.title = 'You must enter a title.';
-  }
-
-  if (!formValues.description) {
-    errors.description = 'You must enter a description.';
-  }
-
-  return errors;
-};
-
-const formWrapped = reduxForm({
-  form: 'postCreate',
-  validate: validate
-})(PostCreate);
-
-export default connect(null, { createPost })(formWrapped);
+export default connect(null, { createPost })(PostCreate);
