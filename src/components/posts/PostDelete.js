996c19bc54c0f8045624324c5acc3393a322d051
Jamie Pham <jtkp66@gmail.com>
2019-02-24 20:15:25 -0800

    Refactored modal to be reuseable, and wired it up to delete post component.

--- src/components/posts/PostDelete.js
+++ src/components/posts/PostDelete.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Modal from '../Modal';
+import history from '../../history';
 
-export default class PostDelete extends Component {
-  render() {
+const PostDelete = () => {
+  const actions = (
+    <React.Fragment>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </React.Fragment>
+  )
+
     return (
       <div>
         Post Delete
-        <Modal />
+        <Modal
+          title="Delete Post"
+          content="Are you sure you want to delete this Post"
+          actions={actions}
+          onDismiss={() => history.push('/')}
+        />
       </div>
     )
-  }
 }
+
+export default PostDelete;
